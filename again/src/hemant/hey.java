e14e52d453a218e6aa3a0776cfcd80ed458813d1
Hemant <Hemant@DESKTOP-D8I7J57>
2020-05-30 09:08:24 +0530

    Commiting it for git

---
+++ again/src/hemant/hey.java
@@ -0,0 +1,18 @@
+package hemant;
+class hey{
+	public static void main(String[] args) {
+		buddies t= new buddies();
+		t.display(10);}
+	
+}
+
+class buddies{
+	
+	void display(int i){
+		System.out.println(i);}
+	
+	
+		}
+	
+
+
